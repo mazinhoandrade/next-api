1300d4264f90a8bd10684f80e55e9b94c911449f
Mazinho <irismarandrade94@gmail.com>
2025-10-05 15:02:34 -0300

    fix: expiration 5min

--- app/api/create_payment/route.ts
+++ app/api/create_payment/route.ts
@@ -33,7 +33,7 @@ export async function POST(req: NextRequest) {
     }
 
     const idempotencyKey = crypto.randomUUID();
- const expiration = new Date(Date.now() + 3 * 60 * 1000).toISOString(); // ⏱️ expira em 3 minutos
+ const expiration = new Date(Date.now() + 5 * 60 * 1000).toISOString(); // ⏱️ expira em 3 minutos
  const response = await fetch("https://api.mercadopago.com/v1/payments", {
   method: "POST",
   headers: {
@@ -52,7 +52,7 @@ export async function POST(req: NextRequest) {
 });
 
 const orderData = await response.json();
-console.log(orderData);
+
 if (!response.ok) {
   console.error("Erro Mercado Pago:", orderData);
   return NextResponse.json({ error: "Erro na criação do pagamento", detail: orderData }, { status: 500 });
