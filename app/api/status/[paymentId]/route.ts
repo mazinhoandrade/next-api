309e231b9fc238efed5ce6607dbd85eed5f28e9a
Mazinho <irismarandrade94@gmail.com>
2025-10-05 13:12:14 -0300

    fix: status

--- app/api/status/[paymentId]/route.ts
+++ app/api/status/[paymentId]/route.ts
@@ -3,11 +3,21 @@
 import { prisma } from "@/app/lib/prisma";
 import { NextResponse } from "next/server";
 
-export async function GET(req: Request, { params }: { params: { paymentId: string } }) {
+interface Params {
+  paymentId: string;
+}
+
+export async function GET(req: Request, context: { params: Params }) {
+  const { params } = context;
   const { paymentId } = params;
 
-  const order = await prisma.order.findUnique({ where: { paymentId } });
-  if (!order) return NextResponse.json({ error: "Pedido não encontrado" }, { status: 404 });
+  const order = await prisma.order.findUnique({
+    where: { paymentId },
+  });
+
+  if (!order) {
+    return NextResponse.json({ error: "Pedido não encontrado" }, { status: 404 });
+  }
 
   return NextResponse.json(order);
 }
